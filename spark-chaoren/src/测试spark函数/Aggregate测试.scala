dba3ae8187a244ba14611a63a74e22492e21559d
15801001455 <15801001455@126.com>
2019-07-03 20:00:13 +0800

    单词计数 及 map和flatMap区别

--- spark-chaoren/src/测试spark函数/Aggregate测试.scala
+++ spark-chaoren/src/测试spark函数/Aggregate测试.scala
@@ -2,13 +2,24 @@ package 测试spark函数
 
 import org.apache.spark.{SparkConf, SparkContext}
 
+/**
+  * aggregate 本地模式
+  *
+  * //分析：
+  * 初始值为2(再求最大值和最后相加部分都会加上初始值2这个因子)，
+  *   partition 0 求最大值（2,1,2,3）得到最大值 3
+  *   partition 1 求最大值（2,4,5,6,7）得到最大值 7
+  *   全局聚合 2+3+7=12
+  */
 object Aggregate测试 {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf()
-
+    conf.setMaster("local")
+    conf.setAppName("LocalSpark")
     val sc = new SparkContext(conf)
-    val z = sc.parallelize(List(1,2,3,4,5,6),2)
-    println(z.aggregate(5)(math.max(_,_),_+_))
+    val z = sc.parallelize(List(1,2,3,4,5,6,7),2)
+    //val z = sc.parallelize(List(1,2,3,4,5,6))
+    println(z.aggregate(2)(math.max(_,_),_+_))//结果是：
   }
 
 }
---
+++ spark-chaoren/src/测试spark函数/aggregateByKey测试.scala
@@ -0,0 +1,28 @@
+package 测试spark函数
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * 作用于相同key的value,初始值只作用于局部聚合，而不作用于全局聚合
+  *
+  * // 底层调用了combineByKey
+  * def aggregateByKey[U](zeroValue: U)(seqOp: (U, V) ⇒ U, combOp: (U, U) ⇒ U)(implicit arg0: ClassTag[U]): RDD[(K, U)]
+  * // zeroValue: U 自定义的初始值
+  * // (seqOp: (U, V) ⇒ U 局部聚合函数，返回类型为U类型
+  * // combOp: (U, U) ⇒ U 全局聚合，返回类型为U类型
+  * def aggregateByKey[U](zeroValue: U, numPartitions: Int)(seqOp: (U, V) ⇒ U, combOp: (U, U) ⇒ U)(implicit arg0: ClassTag[U]): RDD[(K, U)]
+  * // numPartitions: Int 分区数
+  * def aggregateByKey[U](zeroValue: U, partitioner: Partitioner)(seqOp: (U, V) ⇒ U, combOp: (U, U) ⇒ U)(implicit arg0: ClassTag[U]): RDD[(K, U)]
+  * // partitioner: Partitioner 分区器
+  */
+object aggregateByKey测试 {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+    conf.setMaster("local")
+    conf.setAppName("LocalSpark")
+    val sc = new SparkContext(conf)
+    val pairRDD = sc.parallelize(List(("cat",2),("dog",4),("mouse",3),("cat",15)),2)
+    val tuples = pairRDD.aggregateByKey(0)(math.max(_,_),_+_).collect()
+
+  }
+}
