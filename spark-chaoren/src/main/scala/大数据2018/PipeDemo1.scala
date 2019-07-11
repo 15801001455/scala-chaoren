816c11533c79875842939b18898a8b052d7b017f
15801001455 <15801001455@126.com>
2019-07-11 14:46:28 +0800

    数据倾斜解决

--- spark-chaoren/src/main/scala/大数据2018/PipeDemo1.scala
+++ spark-chaoren/src/main/scala/大数据2018/PipeDemo1.scala
@@ -3,7 +3,7 @@ package 大数据2018
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
-  *
+  *  Pipe实验没有成功,以后再看
   */
 object PipeDemo1 {
     def main(args: Array[String]): Unit = {
@@ -13,7 +13,7 @@ object PipeDemo1 {
 
         val sc = new SparkContext(conf)
         val rdd = sc.parallelize(Array("file:///d:","file:///e:","file:///f:",3))
-        val rdd0 = rdd.pipe("ls ")
+        val rdd0 = rdd.pipe("dir ")
         rdd0.collect().foreach(println)
     }
 }
