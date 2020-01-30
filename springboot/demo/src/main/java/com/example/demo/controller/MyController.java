55be3aa0f39a6fd234403f5721048132eb7b929e
15801001455 <15801001455@126.com>
2020-01-30 23:23:27 +0800

    springboot初步

---
+++ springboot/demo/src/main/java/com/example/demo/controller/MyController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.domain.Person;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping(value = "/api",produces = MediaType.APPLICATION_JSON_VALUE)
+public class MyController {
+
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    public Person test(){
+        Person p = new Person();
+        p.setId(3);
+        p.setName("张三");
+        p.setBirthday(new Date());
+        return p;
+    }
+}
