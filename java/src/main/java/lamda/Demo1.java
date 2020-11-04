b5fa6064d9e74089a32ac36711f78f46d30b5213
15801001455 <15801001455@126.com>
2020-11-04 16:06:13 +0800

    Collectors.toMap学习

--- java/src/main/java/lamda/Demo1.java
+++ java/src/main/java/lamda/Demo1.java
@@ -1,9 +1,30 @@
 package lamda;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Demo1 {
 
-    public static void main(String[] args) {
-        System.out.println(16 & 1);
+    //模拟 Map<Long, AgentCloudCustomerVO.AgentCloudCustomer> clientCountMap =
+    // clientCountDetail.stream().collect(Collectors.toMap(t -> isGroupByBid ? t.getBid() : t.getDeptbcid(), t -> t, (k1, k2) -> k2));
+    @Test
+    public void testLamda1() {
+        List<Person> p = new ArrayList<>();
+        p.add(new Person(1,"张三"));
+        p.add(new Person(2,"李四"));
+        p.add(new Person(3,"王五"));
+        p.add(new Person(3,"赵柳"));
+//        Map<Integer, Person> collect = p.stream().collect(Collectors.toMap(t -> t.getId(), t -> t, (k1, k2) -> k2));
+        Map<Integer, Person> collect = p.stream().collect(Collectors.toMap(t -> t.getId(), t -> t, (k1, k2) -> k1));//第三个参数就是遇到相同的key的时候比较的
+//        Map<Integer, Person> collect = p.stream().collect(Collectors.toMap(t -> t.getId(), t -> t));//有重复的key的时候就报错了这里
+        for(Integer key : collect.keySet()){
+            Person person = collect.get(key);
+            System.out.println(person.getId() + "," + person.getName());
+        }
     }
 
 }
---
+++ java/src/main/java/lamda/Person.java
@@ -0,0 +1,15 @@
+package lamda;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person implements Serializable {
+    private Integer id;
+    private String name;
+}
